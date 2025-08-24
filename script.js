cd1b34d59be45bc2ca4f48d872fb72a1b3da9439
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-24 23:50:50 +0600

    added hoeme page navbar and banner

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ document.getElementById('loginButton')
     // Prevent the default form submission
     event.preventDefault();
     console.log("login button clicked");
+
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById('mobile-number').value;
+    const pinNumberValue = document.getElementById('pin-number').value;
+
+    const mobileNumberConverted = parseInt(mobileNumberValue);
+    const pinNumberConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberConverted === mobileNumber && pinNumberConverted === pinNumber) {
+        window.location.href = "./home.html";
+    } else {
+        alert("Invalid mobile number or PIN. Please try again.");
+    }
 })
