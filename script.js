56ac5164cfef7ac6cde31f79d28f3e5c662651d0
Coder-the-noob <moinmatubbar6@gmail.com>
2025-08-24 22:05:20 +0600

    added the login page

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('loginButton')
+.addEventListener('click', function(event){
+    // Prevent the default form submission
+    event.preventDefault();
+    console.log("login button clicked");
+})
